7db7e39a0c5eab19d8f66b6653d9966d1f8f4d49
rmust <ramunas.musteik@gmail.com>
2024-02-05 21:44:26 +0200

    add more headers

--- app.js
+++ app.js
@@ -5,18 +5,23 @@ const PORT = 3000;
 
 const METEO_BASE_URL = "https://api.meteo.lt/v1";
 
+function setHeaders(res) {
+	res.setHeader('Content-Type', 'application/json'); 
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', 'meteo-app.vercel.app')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+}
+
 app.get('/places/:place/forecasts/long-term', async (req, res) => { 
-	const response = await fetch(`${METEO_BASE_URL}/places/${req.params.place}/forecasts/long-term`)
-	const json = await response.json()
-	res.set('Content-Type', 'application/json'); 
-	res.set('Access-Control-Allow-Origin', '*'); 
+  setHeaders(res);
+	const response = await fetch(`${METEO_BASE_URL}/places/${req.params.place}/forecasts/long-term`);
+	const json = await response.json();
 	res.status(200).send(json); 
 }); 
 
 app.get('/places', async (req, res)=>{ 
-	const data = await fetch(`${METEO_BASE_URL}/places`).then(res => res.json())
-	res.set('Content-Type', 'application/json'); 
-	res.set('Access-Control-Allow-Origin', '*'); 
+  setHeaders(res);
+	const data = await fetch(`${METEO_BASE_URL}/places`).then(res => res.json());
 	res.status(200).send(data); 
 }); 
 
