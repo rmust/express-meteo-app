8646a17889651a93ff7aa11a85abae39d83b7e32
rmust <ramunas.musteik@gmail.com>
2024-02-05 21:26:31 +0200

    fix lint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 const METEO_BASE_URL = "https://api.meteo.lt/v1";
 
-app.get('/places/:place/forecasts/long-term', async (req, res)=>{ 
+app.get('/places/:place/forecasts/long-term', async (req, res) => { 
 	const response = await fetch(`${METEO_BASE_URL}/places/${req.params.place}/forecasts/long-term`)
 	const json = await response.json()
 	res.set('Content-Type', 'application/json'); 
@@ -14,19 +14,19 @@ app.get('/places/:place/forecasts/long-term', async (req, res)=>{
 }); 
 
 app.get('/places', async (req, res)=>{ 
-	const data = await fetch(`${METEO_BASE_URL}/places`).then(res=>res.json())
+	const data = await fetch(`${METEO_BASE_URL}/places`).then(res => res.json())
 	res.set('Content-Type', 'application/json'); 
 	res.set('Access-Control-Allow-Origin', '*'); 
 	res.status(200).send(data); 
 }); 
 
-app.listen(PORT, (error) =>{ 
-	if(!error)  {
-		console.log(`Server is Successfully Running, and App is listening on port ${PORT}`) 
-	}
-	else {
-		console.log("Error occurred, server can't start", error); 
-	}
-} 
+app.listen(PORT, (error) => { 
+  	if(!error)  {
+  		console.log(`Server is Successfully Running, and App is listening on port ${PORT}`) 
+  	}
+  	else {
+  		console.log("Error occurred, server can't start", error); 
+  	}
+  } 
 ); 
 
